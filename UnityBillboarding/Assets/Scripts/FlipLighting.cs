c219c4877cff0d1ea4ecfbae859723dcaf1422c3
swaparcher <hydrazhang@hotmail.com>
2017-01-21 21:23:29 -0800

    modified players to not rotate

---
+++ UnityBillboarding/Assets/Scripts/FlipLighting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipLighting : MonoBehaviour {
+
+	private GameObject target;
+
+	// if the current object's Z is less than the target's Z, rotate it on the y axis by 150 
+
+	void Start () {
+		target = GameObject.FindWithTag("Player");
+	}
+
+	void Update () {
+		if (transform.position.z < target.transform.position.z) {
+			Debug.Log(target.transform.position.z);
+			// Debug.Log(transform.position.z);
+			//transform.eulerAngles = new Vector3(0, 180, 0);
+		} else {
+			//transform.eulerAngles = new Vector3(0, 0, 0);
+		}
+	}
+}
