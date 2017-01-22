03de07c0b6f12415ffbf98277274ca934aef3f3f
SevernayaZemlya <jay.aguera@gmail.com>
2017-01-21 20:50:47 -0800

    basic sounds and stuff

--- UnityBillboarding/Assets/Scripts/PlayerMovement.cs
+++ UnityBillboarding/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
@@ -7,12 +8,23 @@ public class PlayerMovement : MonoBehaviour {
 	
 	Vector3 movement;                   // The vector to store the direction of the player's movement.
 	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+
+
+	public AudioSource m_MovementSound;
+
+	public Text countText;
+	public Text winText;
+	private int count;
 	
 	void Awake ()
 	{
 		
 		// Set up references.
 		playerRigidbody = GetComponent <Rigidbody> ();
+		m_MovementSound.Pause();
+
+		count = 0;
+		//m_MovementSound.loop = true;
 	}
 	
 	
@@ -24,6 +36,18 @@ public class PlayerMovement : MonoBehaviour {
 		
 		// Move the player around the scene.
 		Move (h, v);
+
+
+		if (playerRigidbody.velocity.magnitude > 0 && m_MovementSound.isPlaying == false)
+		{
+			m_MovementSound.UnPause();
+		}
+
+		if (playerRigidbody.velocity.magnitude <= 0.1 && m_MovementSound.isPlaying == true)
+		{
+			m_MovementSound.Pause();
+		}
+
 	}
 	
 	void Move (float h, float v)
@@ -42,22 +66,27 @@ public class PlayerMovement : MonoBehaviour {
 	{
 		 if (other.gameObject.tag == "Food") 
 		{
-			other.gameObject.SetActive (false);
+			Destroy(other.gameObject);
+
+			count = count + 1;
+
+
+			//other.gameObject.SetActive (false);
 			//play nom sound
-			//count = count + 1; ?
+			//
 			//SetCountText ();
 		}
 	}
 
 	void SetCountText ()
 	{
-	/*
+
 		countText.text = "Count: " + count.ToString ();
 		if (count >= 21) 
 		{
 			winText.text = "You WIN!";
 		}
-		*/
+
 	}
 
 }
